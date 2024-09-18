387c7c5dc3116486b1db113654b21765d0fb035f
Julian Safadi <julianmatiassafadi@hotmail.com>
2024-09-18 16:08:36 -0300

    dtos - user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,16 @@
+import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
-@Controller()
+@Controller('/users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
-  @Get('/users')
+  @Get()
   getAllUsers() {
     return this.usersService.getUsers()
+  }
+  @Post()
+  createUser(@Body() user: CreateUserDto) {
+    return this.usersService.createUser(user)
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
-  private users = [
+  private users: any[] = [
     {
       id: 1,
       name: 'John Doe',
@@ -16,5 +17,11 @@ export class UsersService {
   ]
   getUsers() {
     return this.users
+  }
+  createUser(user: CreateUserDto) {
+    return {
+      ...user,
+      id: this.users.length + 1
+    }
   }
 }
